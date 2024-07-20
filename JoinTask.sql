245341541f6e1e47eaa3448c5b3ebae20a61e667
afetism <aismayilova1215@gmail.com>
2024-07-20 18:44:01 +0400

    commit

--- JoinTask.sql
+++ JoinTask.sql
@@ -1,14 +1,55 @@
 --8. Display all visitors to the library (and students and teachers) and the books they took.
---8. Kitabxanan?n bütün ziyar?tçil?rini v? onlar?n götürdüyü kitablar? ç?xar?n.
+--8. Kitabxanan?n bütün ziyar?tçil?rini v? ONlar?n götürdüyü kitablar? ç?xar?n.
+SELECT FirstName+' '+LAStName AS 'FullName of Visitors',Books.Name
+FROM S_Cards JOIN Students ON Students.Id=S_Cards.Id_Student
+             JOIN Books ON Books.Id=S_Cards.Id_Book
+UNION ALL
+SELECT FirstName+' '+LAStName AS 'FullName of Visitors',Books.Name
+FROM T_Cards JOIN Teachers ON Teachers.Id=T_Cards.Id_Teacher
+             JOIN Books ON Books.Id=T_Cards.Id_Book
 
---9. Print the most popular author (s) among students and the number of books of this author taken in the library.
---9. Studentl?r aras?nda ?n m??hur author(lar) v? onun(lar?n) götürülmü? kitablar?n?n say?n? ç?xar?n.
 
---10. Print the most popular author (s) among the teachers and the number of books of this author taken in the library.
---10.T?l?b?l?r aras?nda ?n m??hur author(lar) v? onun(lar?n) götürülmü? kitablar?n?n say?n? ç?xar?n.
+--9. Print the most popular author (s) amONg students and the number of books of this author taken in the library.
+--9. Studentl?r arAS?nda ?n m??hur author(lar) v? ONun(lar?n) götürülmü? kitablar?n?n say?n? ç?xar?n.
+WITH TotalCountOfAuthor_T AS
+(
+SELECT Authors.Id,COUNT(Authors.Id) AS  CountOfAuthor
+FROM T_Cards JOIN Books ON T_Cards.Id_Book=Books.Id
+             JOIN Authors ON Books.Id_Author=Authors.Id
+GROUP BY Authors.Id
+),
+MaxCountofAuthor AS
+(
+SELECT MAX(CountOfAuthor)  AS MaxAuthor
+FROM TotalCountOfAuthor_T
+)
 
---11. To deduce the most popular subjects (and) among students and teachers.
---11. Student v? Teacherl?r aras?nda ?n m??hur mövzunu(lar?) ç?xar?n.
+SELECT Authors.Id 'Id Of Author' ,Authors.FirstName+' '+Authors.LAStName 'FullName of Author'
+FROM TotalCountOfAuthor_T JOIN Authors ON TotalCountOfAuthor_T.Id=Authors.Id
+                          JOIN MaxCountofAuthor ON MaxCountofAuthor.MaxAuthor=TotalCountOfAuthor_T.CountOfAuthor
+
+--10. Print the most popular author (s) amONg the teachers and the number of books of this author taken in the library.
+--10.T?l?b?l?r arAS?nda ?n m??hur author(lar) v? ONun(lar?n) götürülmü? kitablar?n?n say?n? ç?xar?n.
+WITH TotalCountOfAuthor AS
+(
+SELECT Authors.Id,COUNT(Authors.Id) AS  CountOfAuthor
+FROM S_Cards JOIN Books ON S_Cards.Id_Book=Books.Id
+             JOIN Authors ON Books.Id_Author=Authors.Id
+GROUP BY Authors.Id
+),
+MaxCountofAuthor AS
+(
+SELECT MAX(CountOfAuthor)  AS MaxAuthor
+FROM TotalCountOfAuthor
+)
+
+SELECT Authors.Id 'Id Of Author' ,Authors.FirstName+' '+Authors.LAStName 'FullName of Author'
+FROM TotalCountOfAuthor JOIN Authors ON TotalCountOfAuthor.Id=Authors.Id
+                        JOIN MaxCountofAuthor ON MaxCountofAuthor.MaxAuthor=TotalCountOfAuthor.CountOfAuthor
+
+
+--11. To deduce the most popular subjects (and) amONg students and teachers.
+--11. Student v? Teacherl?r arAS?nda ?n m??hur mövzunu(lar?) ç?xar?n.
 WITH TotalCountofThems AS (
 SELECT Id_Themes, SUM(Count) AS TotalCount
 FROM (
@@ -49,6 +90,20 @@ FROM T_Cards) AS Vis
 
 --13. If you count the total number of books in the library for 100%, then you need to calculate how many books (in percentage terms) each faculty took.
 --13. ?g?r bütün kitablar?n say?n? 100% q?bul ets?k, siz h?r fakult?nin neç? faiz kitab götürdüyünü hesablamal?s?n?z.
+WITH BookQuantityofEachFaculty AS(
+SELECT Id_Faculty ,COUNT(Id_Book) AS QuantOfBook
+FROM S_Cards JOIN Students ON S_Cards.Id_Student =Students.Id
+             JOIN Groups ON Students.Id_Group=Groups.Id
+			 JOIN Faculties ON Faculties.Id=Groups.Id_Faculty
+GROUP BY Id_Faculty
+),
+
+TotalCountofBook AS (SELECT SUM(Quantity) AS TotalQuantity
+                     FROM Books)
+
+SELECT Id_Faculty,QuantOfBook*100/TotalQuantity AS "%"
+FROM TotalCountofBook T ,BookQuantityofEachFaculty QF
+
 
 
 --14. Display the most reading faculty and the most reading chair.
@@ -68,14 +123,14 @@ FROM St_Faculty
 )
 
 
-select Faculties.Id,Faculties.Name
-from St_Faculty JOIN MAXFACULTY ON St_Faculty.CountOfFacul=MAXFACULTY.MaxFacul
+SELECT Faculties.Id,Faculties.Name
+FROM St_Faculty JOIN MAXFACULTY ON St_Faculty.CountOfFacul=MAXFACULTY.MaxFacul
                 JOIN Faculties ON Faculties.Id=St_Faculty.Id_Faculty
 
 
 
---15. Show the author (s) of the most popular books among teachers and students.
---15. T?l?b?l?r v? mü?lliml?r aras?nda ?n m??hur authoru ç?xar?n.
+--15. Show the author (s) of the most popular books amONg teachers and students.
+--15. T?l?b?l?r v? mü?lliml?r arAS?nda ?n m??hur authoru ç?xar?n.
 WITH TotalBook AS (
 SELECT Id_Book, SUM(BookCount) AS TotalCount
 FROM (SELECT Id_Book, COUNT(Id_Book) AS BookCount
@@ -95,13 +150,13 @@ FROM TotalBook
 
  
 
-SELECT Authors.Id,Authors.FirstName+' '+Authors.LastName
+SELECT Authors.Id,Authors.FirstName+' '+Authors.LAStName
 FROM TotalBook
 JOIN Books ON TotalBook.Id_Book = Books.Id
 JOIN MaxCount ON TotalBook.TotalCount = MaxCount.MaxTotalCount
 JOIN Authors ON Authors.Id=Books.Id_Author
---16. Display the names of the most popular books among teachers and students.
---Mü?llim v? T?l?b?l?r aras?nda ?n m??hur kitablar?n adlar?n? ç?xar?n.
+--16. Display the names of the most popular books amONg teachers and students.
+--Mü?llim v? T?l?b?l?r arAS?nda ?n m??hur kitablar?n adlar?n? ç?xar?n.
 
 
 WITH TotalBook AS (
@@ -128,21 +183,21 @@ JOIN MaxCount ON TotalBook.TotalCount = MaxCount.MaxTotalCount;
 
 --17. Show all students and teachers of designers.
 --17. Dizayn sah?sind? olan bütün t?l?b? v? mü?lliml?ri ekrana ç?xar?n.
-SELECT Teachers.FirstName+' '+Teachers.LastName
+SELECT Teachers.FirstName+' '+Teachers.LAStName
 FROM  Teachers  JOIN Departments ON Teachers.Id_Dep=Departments.Id   
-where Departments.Id=2
-Union 
-SELECT Students.FirstName+' '+Students.LastName
+WHERE Departments.Id=2
+UniON 
+SELECT Students.FirstName+' '+Students.LAStName
 FROM Students JOIN Groups ON Students.Id_Group=Groups.Id
               JOIN Faculties ON Groups.Id_Faculty=Faculties.Id
 WHERE Faculties.Id=2
 
---18. Show all information about students and teachers who have taken books.
---18. Kitab götür?n t?l?b? v? mü?lliml?r haqq?nda informasiya ç?xar?n.
-SELECT Teachers.FirstName+' '+Teachers.LastName
+--18. Show all informatiON about students and teachers who have taken books.
+--18. Kitab götür?n t?l?b? v? mü?lliml?r haqq?nda informASiya ç?xar?n.
+SELECT Teachers.FirstName+' '+Teachers.LAStName
 FROM  Teachers  JOIN T_Cards ON Teachers.Id=T_Cards.Id_Teacher         
-Union 
-SELECT Students.FirstName+' '+Students.LastName
+UniON 
+SELECT Students.FirstName+' '+Students.LAStName
 FROM Students JOIN S_Cards ON Students.Id=S_Cards.Id_Book
             
 --19. Show books that were taken by both teachers and students.
@@ -150,7 +205,7 @@ FROM Students JOIN S_Cards ON Students.Id=S_Cards.Id_Book
 SELECT Books.Name
 FROM  Teachers  JOIN T_Cards ON Teachers.Id=T_Cards.Id_Teacher
                 JOIN Books ON Books.Id=T_Cards.Id_Book
-Union 
+UniON 
 SELECT Books.Name
 FROM Students JOIN S_Cards ON Students.Id=S_Cards.Id_Book
               JOIN Books ON Books.Id=S_Cards.Id_Book
